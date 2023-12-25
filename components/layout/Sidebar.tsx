8732a4f71adabcfe4fb5853219df7dfcebb0ca63
alphacoder-mp3 <shindeakshay223@gmail.com>
2023-12-25 17:14:03 +0530

    commiting my first progress

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -10,7 +10,7 @@ import SidebarLogo from './SidebarLogo';
 import SidebarTweetButton from './SidebarTweetButton';
 
 const Sidebar = () => {
-//   const { data: currentUser } = useCurrentUser();
+  //   const { data: currentUser } = useCurrentUser();
 
   const items = [
     {
@@ -23,38 +23,43 @@ const Sidebar = () => {
       label: 'Notifications',
       href: '/notifications',
       auth: true,
-    //   alert: currentUser?.hasNotification
+      //   alert: currentUser?.hasNotification
     },
     {
       icon: FaUser,
       label: 'Profile',
-    //   href: `/users/${currentUser?.id}`,
+      //   href: `/users/${currentUser?.id}`,
       href: '/users/123',
       auth: true,
     },
-  ]
+  ];
 
   return (
     <div className="col-span-1 h-full pr-4 md:pr-6">
-        <div className="flex flex-col items-end">
-          <div className="space-y-2 lg:w-[230px]">
-            <SidebarLogo />
-            {items.map((item) => (
-              <SidebarItem
-                key={item.href}
-                alert={item.alert}
-                auth={item.auth}
-                href={item.href} 
-                icon={item.icon} 
-                label={item.label}
-              />
-            ))}
-            {/* {currentUser && <SidebarItem onClick={() => signOut()} icon={BiLogOut} label="Logout" />} */}
-            <SidebarTweetButton />
-          </div>
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <SidebarLogo />
+          {items.map(item => (
+            <SidebarItem
+              key={item.href}
+              // alert={item.alert}
+              auth={item.auth}
+              href={item.href}
+              icon={item.icon}
+              label={item.label}
+            />
+          ))}
+          {/* {currentUser && <SidebarItem onClick={() => signOut()} icon={BiLogOut} label="Logout" />} */}
+          <SidebarItem
+            // onClick={() => signOut()}
+            icon={BiLogOut}
+            label="Logout"
+          />
+          <SidebarTweetButton />
         </div>
       </div>
-  )
+    </div>
+  );
 };
 
 export default Sidebar;
