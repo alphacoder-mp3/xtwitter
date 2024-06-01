c30c07dabb8a345943edd035a7174282ab27a2c5
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-06-02 04:18:24 +0530

    Fixes

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,43 +1,40 @@
+import useUser from '@/hooks/useUser';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { useCallback } from 'react';
-
-import useUser from '@/hooks/useUser';
-
-interface AvatarProps {
-  userId: string;
+import React, { useCallback } from 'react';
+interface IAvatar {
+  userId: number;
   isLarge?: boolean;
   hasBorder?: boolean;
 }
 
-const Avatar: React.FC<AvatarProps> = ({ userId, isLarge, hasBorder }) => {
-  const router = useRouter();
-
+const Avatar: React.FC<IAvatar> = ({ userId, isLarge, hasBorder }) => {
   const { data: fetchedUser } = useUser(userId);
+  const router = useRouter();
 
   const onClick = useCallback(
     (event: any) => {
       event.stopPropagation();
 
       const url = `/users/${userId}`;
-
       router.push(url);
     },
-    [router, userId]
+    [userId, router]
   );
 
   return (
     <div
+      onClick={onClick}
       className={`
-        ${hasBorder ? 'border-4 border-black' : ''}
-        ${isLarge ? 'h-32' : 'h-12'}
+        ${hasBorder ? 'border-4 border-black' : ''} 
+        ${isLarge ? 'h-32' : 'h-12'} 
         ${isLarge ? 'w-32' : 'w-12'}
-        rounded-full 
-        hover:opacity-90 
-        transition 
+        rounded-full
+        hover:opacity-90
+        transition
         cursor-pointer
         relative
-      `}
+    `}
     >
       <Image
         fill
