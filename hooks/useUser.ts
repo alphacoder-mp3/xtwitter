b68674182a2004325193ee420fdb7f5fdbbc6647
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-06-02 04:15:14 +0530

    Updates

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -2,7 +2,7 @@ import useSWR from 'swr';
 
 import fetcher from '@/libs/fetcher';
 
-const useUser = (userId: string) => {
+const useUser = (userId: number | null) => {
   const { data, error, isLoading, mutate } = useSWR(
     userId ? `/api/users/${userId}` : null,
     fetcher
